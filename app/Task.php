8138db42b4ae1c4fe6086d6a311d7e52616633ca
d3z41k <d3z41k@gmail.com>
2017-10-18 13:24:44 +0200

    complite

---
+++ app/Task.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App;
+
+use App\User;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    /**
+     * Массово присваиваемые атрибуты.
+     *
+     * @var array
+     */
+    protected $fillable = ['name', 'user_id'];
+
+    /**
+     * Получить все задачи пользователя.
+     */
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
+
+    /**
+     * Получить пользователя - владельца данной задачи
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
